9401fd61bc13039a6486b71363854aa28549502c
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-07 23:22:22 -0700

    commit tree d66c752fff42e37cde5d802ffe27d26f8eceec99

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2);
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 16
+let num4 = 6
+const mod = num3 % num4;
+
+let max = Math.max(1,2,20);
